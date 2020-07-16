ab8ec8e7d460dec152afc2c57047c0dd0b039572
Christopher W H Davis <chrdavi@microsoft.com>
2020-07-16 08:37:01 -0700

    debugging

--- getfcm/index.js
+++ getfcm/index.js
@@ -12,38 +12,51 @@ module.exports = function (context, req) {
             body: "You must post inputs to this service"
         };
     } else {
-        let inputs = req.body; 
-        //console.log(inputs);
-        let cDate = "";
-        if(Object.prototype.toString.call(inputs.createDate === '[object Date]')) {
-            cDate = inputs.createDate.toISOString();
-        } else {
-            cDate = inputs.createDate;
-        }
-        //console.log(Object.prototype.toString.call(inputs.createDate));
-        //console.log(Object.prototype.toString.call(cDate));
-        //Object.prototype.toString.call(date) === '[object Date]'
-        let concepts = [];
-        let connections = [];
+        try {
 
-        client.open().then(() =>{
-            client.submit("g.V().has('modelName', name).has('createDate', createDate).bothE().otherV().path()", { name: inputs.modelName, createDate: cDate }).then(function (result) {
-                //console.log("Result: %s\n", JSON.stringify(result._items)); 
+            let inputs = req.body; 
+            //console.log(inputs);
+            let cDate = "";
+            if(Object.prototype.toString.call(inputs.createDate === '[object Date]')) {
+                context.log('Changing date...');
+                cDate = inputs.createDate.toISOString();
+            } else {
+                context.log('Date already a string...');
+                cDate = inputs.createDate;
+            }
+            //console.log(Object.prototype.toString.call(inputs.createDate));
+            //console.log(Object.prototype.toString.call(cDate));
+            //Object.prototype.toString.call(date) === '[object Date]'
+            let concepts = [];
+            let connections = [];
 
-                concepts = structureBuilder.buildConcepts(result._items, inputs);
-                connections = structureBuilder.buildConnections(result._items, concepts);
+            client.open().then(() =>{
+                context.log('Cosmos time...');
+                client.submit("g.V().has('modelName', name).has('createDate', createDate).bothE().otherV().path()", { name: inputs.modelName, createDate: cDate }).then(function (result) {
+                    //console.log("Result: %s\n", JSON.stringify(result._items)); 
 
-                let cytoStructure = structureBuilder.buildCytoStructure(concepts, connections);
-            
-                context.res = {
-                    headers: {
-                        'content-type':'application/json'
-                    },
-                    status: 200,
-                    body: { elements: cytoStructure }
-                };
-                context.done();
+                    concepts = structureBuilder.buildConcepts(result._items, inputs);
+                    connections = structureBuilder.buildConnections(result._items, concepts);
+
+                    let cytoStructure = structureBuilder.buildCytoStructure(concepts, connections);
+                
+                    context.res = {
+                        headers: {
+                            'content-type':'application/json'
+                        },
+                        status: 200,
+                        body: { elements: cytoStructure }
+                    };
+                    context.done();
+                });
             });
-        });
+        } catch (e) { 
+            context.log('Err!');
+            context.log(e);
+            context.res = {
+                status: 400,
+                body: e
+            };
+        }
     }
 };
